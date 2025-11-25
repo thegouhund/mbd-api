bce15cf6f6d4259018a9a33907df5214f04c8cc5
thegouhund <alexandresapu1@gmail.com>
2025-11-25 19:48:48 +0800

    feat: add function role check

--- queries/function.sql
+++ queries/function.sql
@@ -13,4 +13,30 @@ BEGIN
     END IF;
 END$$
 
+CREATE OR REPLACE FUNCTION IsUserEnrolledInCourse(p_user_id INT, p_course_id INT)
+RETURNS BOOLEAN
+DETERMINISTIC
+BEGIN
+    DECLARE v_count INT;
+    SELECT COUNT(*) INTO v_count FROM enrollments WHERE user_id = p_user_id AND course_id = p_course_id;
+    IF v_count > 0 THEN
+        RETURN TRUE;
+    ELSE
+        RETURN FALSE;
+    END IF;
+END$$
+
+CREATE OR REPLACE FUNCTION IsCreatorOfCourse(p_user_id INT, p_course_id INT)
+RETURNS BOOLEAN
+DETERMINISTIC
+BEGIN
+    DECLARE v_count INT;
+    SELECT COUNT(*) INTO v_count FROM courses WHERE creator_id = p_user_id AND course_id = p_course_id;
+    IF v_count > 0 THEN
+        RETURN TRUE;
+    ELSE
+        RETURN FALSE;
+    END IF;
+END$$
+
 DELIMITER ;
